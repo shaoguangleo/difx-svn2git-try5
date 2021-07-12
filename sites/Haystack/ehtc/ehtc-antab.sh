e40e4d0eeb461b0ca2a805f7637682d0dfad136e
gbcrew <gbc@haystack.mit.edu>
2021-07-12 17:17:41 +0000

    Sychronizing with changes for 2021.
    git-svn-id: https://svn.atnf.csiro.au/difx@10062 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/Haystack/ehtc/ehtc-antab.sh
+++ sites/Haystack/ehtc/ehtc-antab.sh
@@ -25,6 +25,7 @@ you need make some adjustments for understanding the plot or producing
 different versions (default values are given):
     decimation=$decimation      use one value in this many
     plotrange=$plotrange        Hours and Tsys for gnuplot
+The plotrange defaults to [][0:10] allowing for nominal Tsys performance.
 "
 # parse arguments
 band=${1-'noband'}
