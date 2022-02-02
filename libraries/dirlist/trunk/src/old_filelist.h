2f1833e916d561db07df50ab20ce3820e3a4c62a
shaoguangleo <sgguo@shao.ac.cn>
2022-02-02 04:15:09 +0000

    Release for D100

--- libraries/dirlist/trunk/src/old_filelist.h
+++
@@ -1,9 +0,0 @@
-#ifndef __OLD_FILELIST_H__
-#define __OLD_FILELIST_H__
-
-#include <sstream>
-#include "dirlist.h"
-
-int loadOldFileList(DirList &D, const char *fileName, std::stringstream &error);
-
-#endif
